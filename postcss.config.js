258623a3431d9efeae477d7aeabd92d20a9c8d32
Nantawat04 <66110693@dpu.ac.th>
2025-11-14 04:48:56 +0700

    p

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    }
-=======
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    }
->>>>>>> d061f4566da284f6e2306614f9626714147ee7e8
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
